b29b47006a827cabcce115f99793fe12059b9bac
EiThandarCho <eithandarcho2019@gmail.com>
2019-05-11 18:13:18 +0630

    first commit

---
+++ app/controllers/controllers.php
@@ -0,0 +1,20 @@
+<?php 
+function _HomeController() {
+	$data = [
+		'title'	=> 'Myanmar Links'
+	];
+	_load_view("index", $data);
+}
+function _BlogController() {
+	$data = [
+		'title'	=> 'Blog Area'
+	];
+	_load_view("index", $data);
+}
+function _TestController() {
+	$data = [
+		'title'	=> 'Test Area'
+	];
+	_load_view("index", $data);
+}
+ ?>
